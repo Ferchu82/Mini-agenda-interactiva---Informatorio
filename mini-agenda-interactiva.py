c800774fa7b377e9904037ec16b42af685053ad1
Fernando E Peró <fernandoepero@hotmail.com>
2025-06-17 15:57:14 -0300

    "Se crea Menú desplegable"

--- mini-agenda-interactiva.py
+++ mini-agenda-interactiva.py
@@ -6,5 +6,17 @@ ventana.title("Mini Agenda Interactiva")
 ventana.geometry("450x350")
 ventana.configure(bg="#f0f0f0")
 
+barra_menu = tk.Menu(ventana)
+ventana.config(menu=barra_menu)
+
+menu_principal = tk.Menu(barra_menu, tearoff=0)
+barra_menu.add_cascade(label="Opciones", menu=menu_principal)
+
+submenu = tk.Menu(menu_principal, tearoff=0)
+menu_principal.add_cascade(label="Tareas", menu=submenu)
+submenu.add_command(label="Agregar nueva")
+submenu.add_command(label="Eliminar tarea")
+
+
 
 ventana.mainloop()
