655b42f97574d0bbc8f9b35310096edbafcc3b33
Fernando E Peró <fernandoepero@hotmail.com>
2025-06-17 15:49:58 -0300

    "Se inicializa la Ventana Principal"

---
+++ mini-agenda-interactiva.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+import time
+
+ventana = tk.Tk()
+ventana.title("Mini Agenda Interactiva")
+ventana.geometry("450x350")
+ventana.configure(bg="#f0f0f0")
+
+
+ventana.mainloop()
